03d9eba5fa5d59b445fcf9663a5979cb76f77bb3
TanmayMachkar <tanmaymacho@gmail.com>
2024-06-25 13:26:28 +0530

    swapping weth tokens with dai tokens using uniswapv3 sdk

---
+++ FetchPrice.js
@@ -0,0 +1,40 @@
+const { ethers } = require('ethers');
+const {abi:QuoterABI} = require('@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json');
+
+//got the link from alchemy dashboard app api(ethereum mainnet) 
+const provider = new ethers.providers.JsonRpcProvider(
+	'https://eth-mainnet.g.alchemy.com/v2/kYpkJ2SIGD-TfnhK9fxgxWz7ASGujf0p'
+);
+
+const fetchPrice = async(addressFrom, addressTo, humanValue) => {
+	const QUOTER_CONTRACT_ADDRESS = '0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6';
+	
+	//both functions from v3 protocol docs
+	const quoterContract = new ethers.Contract(
+	  QUOTER_CONTRACT_ADDRESS,
+	  QuoterABI,
+	  provider
+	);
+
+	const amountIn = ethers.utils.parseUnits(humanValue, 18);
+	const quotedAmountOut = await quoterContract.callStatic.quoteExactInputSingle(
+	  addressFrom,
+	  addressTo,
+	  3000,
+	  amountIn.toString(), //converting big number object(wei) to string
+	  0
+	);
+
+	const amount = ethers.utils.formatUnits(quotedAmountOut.toString(), 18);
+	return amount;
+}
+
+const main = async() => {
+	const addressFrom = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'; //WETH
+	const addressTo = '0x6B175474E89094C44Da98b954EedeAC495271d0F'; //DAI
+
+	const humanValue = '1'; //1 weth since weth is costly
+	const result = await fetchPrice(addressFrom, addressTo, humanValue);
+	console.log(result);
+}
+main();
